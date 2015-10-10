bf2e9bcdfc3e58d897cafd4c34a58af1cfd30e6d
Dmitry <dmitryrudneff@gmail.com>
2015-10-10 10:02:28 +0300

    Refactored 3 and 2

--- algorithm_2_1/main.cpp
+++ algorithm_2_1/main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
+
+void find_indexes_of_max_sum(int* arr_a, int* arr_b, int len, int &first_index, int &second_index);
 
 int main()
 {
 	int n = 0;
 	cin >> n;
-	int arr_a[n];
-	int arr_b[n];
+	int* arr_a = new int[n];
+	int* arr_b = new int[n];
 	for (int i = 0; i < n; ++i) {
 		cin >> arr_a[i];
 	}
@@ -15,29 +18,42 @@ int main()
 		cin >> arr_b[i];
 	}
 
-	int index_in_a = 0;
+	int index_in_a, index_in_b;
+	find_indexes_of_max_sum(arr_a, arr_b, n, index_in_a, index_in_b);
+	cout << index_in_a << ' ' << index_in_b;
+	delete [] arr_a;
+	delete [] arr_b;
+	return 0;
+}
+
+void find_indexes_of_max_sum(int* arr_a, int* arr_b, int len, int &first_index, int &second_index) {
+	if (!arr_a || !arr_b) {
+		throw std::invalid_argument("Null pointer to array.");
+	}
+	first_index = 0;
+	second_index = 0;
 	int index_max_in_a = 0;
-	int index_in_b = 0;
 
-	int max_first_sum = arr_a[index_in_a] + arr_b[index_in_b];
+	int max_first_sum = arr_a[first_index] + arr_b[second_index];
 
-	for (int i = 1; i < n; ++i) {
+	for (int i = 1; i < len; ++i) {
 
+		// ищем максимальный в первом массиве
 		if (arr_a[i] > arr_a[index_max_in_a]) {
 			index_max_in_a = i;
 		}
 
-		if (arr_b[i] > arr_b[index_in_b]) {
-			max_first_sum += -arr_b[index_in_b] + arr_b[i];
-			index_in_b = i;
+		// если текущий во втором массиве больше, чем сохраненный, меняем
+		if (arr_b[i] > arr_b[second_index]) {
+			max_first_sum += -arr_b[second_index] + arr_b[i];
+			second_index = i;
 		}
 
+		// если сумма максимального найденного в первом массиве и i-го во втором больше, меняем
 		if(arr_a[index_max_in_a] + arr_b[i] > max_first_sum) {
-			index_in_a = index_max_in_a;
-			index_in_b = i;
-			max_first_sum = arr_a[index_in_a] + arr_b[index_in_b];
+			first_index = index_max_in_a;
+			second_index = i;
+			max_first_sum = arr_a[first_index] + arr_b[second_index];
 		}
 	}
-	cout << index_in_a << ' ' << index_in_b;
-	return 0;
 }
--- algorithm_3_4/main.cpp
+++ algorithm_3_4/main.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
+int find_neighbour(const int *first_arr, const int len, const int num);
+
 int main()
 {
 
 	int n = 0;
 	cin >> n;
 
-	int first_arr[n];
+	int* first_arr = new int[n];
 	for (int i = 0; i < n; ++i) {
 		cin >> first_arr[i];
 	}
@@ -16,34 +18,13 @@ int main()
 	int m = 0;
 	cin >> m;
 
-	int second_arr[m];
+	int* second_arr = new int[m];
 	for(int i = 0; i < m; ++i) {
 		cin >> second_arr[i];
 	}
 
 	for (int i = 0; i < m; ++i) {
-		int left_border = 0;
-		int right_border = n - 1;
-
-		// бинарным поиском ищем пару элементов, между которыми находится искомый элемент
-		while((right_border - left_border) > 1) {
-			if (second_arr[i] > first_arr[(right_border + left_border) / 2]) {
-
-				left_border = (right_border + left_border) / 2;
-			}
-			else {
-				right_border = (right_border + left_border) / 2;
-			}
-		}
-		// необходима, если есть повторяющиеся элементы в первом массиве
-		if (first_arr[left_border] == first_arr[right_border]) {
-			second_arr[i] = left_border;
-		}
-		// выясняем, к какому элементу ближе расположен искомый
-		else {
-			second_arr[i] = (second_arr[i] - first_arr[left_border] <= first_arr[right_border] - second_arr[i]) ? left_border :
-					right_border;
-		}
+		second_arr[i] = find_neighbour(first_arr, n, second_arr[i]);
 	}
 
 	for (int i = 0; i < m; ++i) {
@@ -51,4 +32,43 @@ int main()
 	}
 
 	return 0;
+}
+
+int find_neighbour(const int *first_arr, const int len, const int num) {
+	int left_border = 0;
+	int right_border = len - 1;
+	for (int j = 0; j*j < len; ++j) {
+		if (num < first_arr[0]) {
+			right_border = 0;
+			break;
+		}
+
+		if (num >= first_arr[left_border] && num <= first_arr[j*j]) {
+			right_border = j*j;
+			break;
+		}
+		else {
+			left_border = j*j;
+		}
+	}
+
+	// бинарным поиском ищем пару элементов, между которыми находится искомый элемент
+	while((right_border - left_border) > 1) {
+		if (num > first_arr[(right_border + left_border) / 2]) {
+
+			left_border = (right_border + left_border) / 2;
+		}
+		else {
+			right_border = (right_border + left_border) / 2;
+		}
+	}
+	// необходима, если есть повторяющиеся элементы в первом массиве
+	if (first_arr[left_border] == first_arr[right_border]) {
+		return left_border;
+	}
+		// выясняем, к какому элементу ближе расположен искомый
+	else {
+		return (num - first_arr[left_border] <= first_arr[right_border] - num) ? left_border :
+						right_border;
+	}
 }
